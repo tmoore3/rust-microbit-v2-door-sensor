2d532a2b4df7a5f19d9ac3ca6d209c48eef8d6a7
Tom Moore <tomwmoore@hotmail.com>
2024-03-31 18:40:21 +0100

    Created a simple "Hello World" app for microbit v2.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+use microbit as _;
+
+use cortex_m::asm::nop;
+use cortex_m_rt::entry;
+use rtt_target::{rprintln, rtt_init_print};
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Hello World!");
+    loop {
+        nop();
+    }
 }
